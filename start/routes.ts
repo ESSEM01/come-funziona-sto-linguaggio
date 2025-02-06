7c90e57f50225fce99ee9821a031feef89060cb1
Stefano Moretti <lav.moretti@gmail.com>
2025-02-06 11:35:21 +0100

    Aggiunta della configurazione iniziale del progetto con file di configurazione, middleware, stili e pagine di errore

---
+++ start/routes.ts
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import app from '@adonisjs/core/services/app'
+import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
+
+router.on('/').render('pages/home').as('home')
+
+router
+    .get('/movies/:slug', async (ctx) => {
+        const url = app.makeURL('resources/movies/${ctx.params.slug}.html')
+        const movie = await fs.readFile(url, 'utf-8')
+        return ctx.view.render('pages/movies', { movie })
+    }).as('movies.show')
+
+// router.get('/movies', () => {}).as('movies.index')
+// router.get('/movies/single-movie', () => {}).as('movies.show')
+// router.get('/movies/create', () => {}).as('movies.create')
+// router.post('/movies', () => {}).as('movies.store')
+// router.get('/movies/dongle-movie/edit', () => {}).as('movies.edit')
+// router.put('/movies/single-movie', () => {}).as('movies.update')
+// router.delete('/movies/single-movie', () => {}).as('movies.destroy')
