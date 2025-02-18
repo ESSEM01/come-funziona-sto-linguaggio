3d70aa88f55ed092bbfe432f3271857b43c5a9b3
Stefano Moretti <lav.moretti@gmail.com>
2025-02-18 15:50:30 +0100

    Aggiunta del modello Profile e del controller ProfilesController; creazione della factory per Profile; aggiornamento delle migrazioni per includere la tabella profiles e l'ordinamento nei cast_movies; aggiunta della proprietà sort_order nella tabella crew_movies.

---
+++ database/factories/profile_factory.ts
@@ -0,0 +1,12 @@
+import factory from '@adonisjs/lucid/factories'
+import Profile from '#models/profile'
+
+export const ProfileFactory = factory
+  .define(Profile, async ({ faker }) => {
+    return {
+
+      userId: 1,
+      description: faker.lorem.sentences(),
+    }
+  })
+  .build()
