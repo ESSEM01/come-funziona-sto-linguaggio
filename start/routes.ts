75416a26c7a27158a58e715a380a25cf9e83ba9c
Stefano Moretti <lav.moretti@gmail.com>
2025-02-24 10:46:07 +0100

    Aggiunta della pagina degli scrittori e della logica per visualizzare gli scrittori e i loro film; aggiornamento della navigazione per includere un link agli scrittori; implementazione del WritersController per gestire la visualizzazione degli scrittori.

--- start/routes.ts
+++ start/routes.ts
@@ -8,8 +8,10 @@
 */
 
 
+
 import router from '@adonisjs/core/services/router'
 const DirectorsController = () => import('#controllers/directors_controller')
+const WritersController = () => import('#controllers/writers_controller')
 const MoviesController = () => import('#controllers/movies_controller') 
 
 router.get('/', [MoviesController, 'index']).as('home')
@@ -21,6 +23,9 @@ router
 
     router.get('directors', [DirectorsController, 'index']).as('directors.index')
     router.get('directors/:id', [DirectorsController, 'show']).as('directors.show')
+
+    router.get('writers', [WritersController, 'index']).as('writers.index')
+    router.get('writers/:id', [WritersController, 'show']).as('writers.show')
 
     router.get('/flush', [MoviesController, 'flushCache'])
 // router.get('/movies', () => {}).as('movies.index')
